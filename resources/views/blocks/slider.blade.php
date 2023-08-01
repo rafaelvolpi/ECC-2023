ce833a9670c2e9d2b1e11bf41e241e8ad71e1de0
Rafael Volpi <rafaelvolpi@gmail.com>
2023-08-01 17:21:12 -0300

    Fix slider block

--- resources/views/blocks/slider.blade.php
+++ resources/views/blocks/slider.blade.php
@@ -27,7 +27,6 @@
 
     $config = get_field('config');
     $slides = get_field('slide');
-    // print_r($slides);
 
     if ($config['autoplay']) {
         $data_glide['autoplay'] = $config['autoplay'] * 1000;
@@ -40,79 +39,122 @@
 
     $data_glide = json_encode( $data_glide );
 @endphp
-<div class="clear-both">
-<section id="{{ $block['id'] }}" class="relative">
-    <div class="glide" data-glide='{{ $data_glide }}'>
-        <div class="glide__track" data-glide-el="track">
-            <ul class="glide__slides">
-                @foreach ($slides['slide'] as $k => $slide)
 
-                    @if ($config['template'] === "hero")
-                        <li class="flex flex-row rounded-xl bg-{{ $slide['bg_color_color'] }} glide__slide">
-                            <div class="w-1/2 rounded-l-xl text-left p-10 flex flex-col gap-8 justify-center">
-                                <h1 class="heading-1 text-ecc-blue">{{ $slide['title'] }}</h1>
-                                @if ($slide['content'])
-                                    <p>{{ $slide['content'] }}</p>
-                                @endif
-                                @if ($slide['link'])
-                                    <span>
-                                        <a
-                                        href="{{ $slide['link']['url'] }}"
-                                        class="btn btn-white"
-                                        >{{ $slide['link']['title'] }}</a>
-                                    </span>
-                                @endif
-                            </div>
-                            
-                            <img
-                                src="{{ $slide['image']['sizes']['medium_large'] }}"
-                                alt="item.image.title"
-                                class="w-1/2 aspect-video object-center object-cover rounded-r-xl"
-                                loading="lazy"
-                            />
-                        </li>
-                    @elseif ($config['template'] === "parceiros")
-                        <li class="glide__slide flex flex-col gap-6 justify-between items-center h-full">
-                            <img
-                                src="{{ $slide['image']['sizes']['medium_large'] }}"
-                                alt="item.image.title"
-                                class="w-3/5 aspect-video object-contain object-center"
-                                loading="lazy"
-                            />
-                            @if ($slide['link'])
-                                    <a
-                                    href="{{ $slide['link']['url'] }}"
-                                    class="btn btn-white"
-                                    >{{ $slide['link']['title'] }}</a>
+@if($slides['slide'])
+
+    @if(count($slides['slide']) === 1)
+
+    <section id="{{ $block['id'] }}" class="relative">
+        <div class="flex flex-row rounded-xl bg-{{ $slides['slide'][0]['bg_color_color'] }} glide__slide">
+            <div class="w-1/2 min-h-[15rem] rounded-l-xl text-left p-10 flex flex-col gap-8 justify-center">
+                <h1 class="heading-1 text-ecc-blue pb-0">{{ $slides['slide'][0]['title'] }}</h1>
+                @if ($slides['slide'][0]['content'])
+                    <p>{{ $slides['slide'][0]['content'] }}</p>
+                @endif
+                @if ($slides['slide'][0]['link'])
+                    <span>
+                        <a
+                        href="{{ $slides['slide'][0]['link']['url'] }}"
+                        class="btn btn-white"
+                        >{{ $slides['slide'][0]['link']['title'] }}</a>
+                    </span>
+                @endif
+            </div>
+            @if ($slides['slide'][0]['image'])
+            <img
+                src="{{ $slides['slide'][0]['image']['sizes']['medium_large'] }}"
+                alt="item.image.title"
+                class="w-1/2 aspect-video object-center object-cover rounded-r-xl"
+                loading="lazy"
+            />
+            @endif
+        </div>
+    </section>
+
+    @else
+
+        <div class="clear-both">
+        <section id="{{ $block['id'] }}" class="relative">
+            <div class="glide" data-glide='{{ $data_glide }}'>
+                <div class="glide__track" data-glide-el="track">
+                    <ul class="glide__slides">
+                        @foreach ($slides['slide'] as $k => $slide)
+
+                            @if ($config['template'] === "hero")
+                                <li class="flex flex-row rounded-xl bg-{{ $slide['bg_color_color'] }} glide__slide">
+                                    <div class="w-1/2 rounded-l-xl text-left p-10 flex flex-col gap-8 justify-center">
+                                        <h1 class="heading-1 text-ecc-blue">{{ $slide['title'] }}</h1>
+                                        @if ($slide['content'])
+                                            <p>{{ $slide['content'] }}</p>
+                                        @endif
+                                        @if ($slide['link'])
+                                            <span>
+                                                <a
+                                                href="{{ $slide['link']['url'] }}"
+                                                class="btn btn-white"
+                                                >{{ $slide['link']['title'] }}</a>
+                                            </span>
+                                        @endif
+                                    </div>
+                                    @if ($slide['image'])
+                                    <img
+                                        src="{{ $slide['image']['sizes']['medium_large'] }}"
+                                        alt="item.image.title"
+                                        class="w-1/2 aspect-video object-center object-cover rounded-r-xl"
+                                        loading="lazy"
+                                    />
+                                    @endif
+                                </li>
+                            @elseif ($config['template'] === "parceiros")
+                                <li class="glide__slide flex flex-col gap-6 justify-between items-center h-full">
+                                    <img
+                                        src="{{ $slide['image']['sizes']['medium_large'] }}"
+                                        alt="item.image.title"
+                                        class="w-3/5 aspect-video object-contain object-center"
+                                        loading="lazy"
+                                    />
+                                    @if ($slide['link'])
+                                            <a
+                                            href="{{ $slide['link']['url'] }}"
+                                            class="btn btn-white"
+                                            >{{ $slide['link']['title'] }}</a>
+                                    @endif
+                                </li>
                             @endif
-                        </li>
+
+                        @endforeach
+                    </ul>
+                </div>
+
+                @if (count($slides['slide']) > 1)
+                    @if (($config['navegacao'] == "bullets") || ($config['navegacao'] == "both"))
+                    <div class="glide__bullets" data-glide-el="controls[nav]">
+                        @foreach ($slides['slide'] as $k => $slide)
+                        <button class="glide__bullet" data-glide-dir="={{ $k }}"></button>
+                        @endforeach
+                    </div>
                     @endif
 
-                @endforeach
-            </ul>
+                    @if (($config['navegacao'] == "arrows") || ($config['navegacao'] == "both"))
+                    <div class="glide__arrows" data-glide-el="controls">
+                        <button class="glide__arrow glide__arrow--left" data-glide-dir="&lt;"></button>
+                        <button class="glide__arrow glide__arrow--right" data-glide-dir="&gt;"></button>
+                    </div>
+                    @endif
+                @endif
+            </div>
+        </section>
         </div>
 
-        @if (count($slides['slide']) > 1)
-            @if (($config['navegacao'] == "bullets") || ($config['navegacao'] == "both"))
-            <div class="glide__bullets" data-glide-el="controls[nav]">
-                @foreach ($slides['slide'] as $k => $slide)
-                <button class="glide__bullet" data-glide-dir="={{ $k }}"></button>
-                @endforeach
-            </div>
-            @endif
+    @endif
 
-            @if (($config['navegacao'] == "arrows") || ($config['navegacao'] == "both"))
-            <div class="glide__arrows" data-glide-el="controls">
-                <button class="glide__arrow glide__arrow--left" data-glide-dir="&lt;"></button>
-                <button class="glide__arrow glide__arrow--right" data-glide-dir="&gt;"></button>
-            </div>
-            @endif
-        @endif
-    </div>
-</section>
-</div>
-<style type="text/css">
-  section#{{ $block['id'] }} {
-    {{ $style }}
-  }
-</style>
+        <style type="text/css">
+            section#{{ $block['id'] }} {
+                {{ $style }}
+            }
+        </style>
+
+@endif
+
+
+
