e8963ffc647ad1bf59d2f60b257f8d6860f48258
Rafael Volpi <rafaelvolpi@gmail.com>
2023-08-24 10:30:44 -0300

    remove calendar block if don't have events

--- resources/views/blocks/home-calendar.blade.php
+++ resources/views/blocks/home-calendar.blade.php
@@ -43,67 +43,76 @@
 
 @endphp
 
+@if(count($eventos) > 0)
 
-<section id="{{ $block['id'] }}">
-    <div class="glide" data-glide='{
-        "loop": true,
-        "perView": 4
-    }'>
-    <div class="glide__track" data-glide-el="track">
-        <ul class="glide__slides">
-            @foreach ($eventos as $evento)
-            
-            <li class="flex flex-col rounded-xl bg-gray-100 glide__slide">
-                <div class="rounded-l-xl text-left p-5 flex flex-col justify-center">
-                    <div class="flex flex-row items-center justify-between">
-                        @if (isset($evento['fields']['date_time']['start_date']))
-                        <div class="h-16 w-24 text-base uppercase rounded-lg border border-blue/20 bg-blue/5 flex flex-col items-center justify-center">
-                            <p class="{{ isset($evento['fields']['date_time']['start_hour']) ? 'border-b border-blue/40' : '' }}">
-                                {{ $evento['fields']['date_time']['start_date'] }}
-                            </p>
-                            @if ($evento['fields']['date_time']['start_hour'])
-                                <p>
-                                    {{ $evento['fields']['date_time']['start_hour'] }}
-                                </p>
-                            @endif
-                        </div>
-                        @endif
-                        
-                        @if (isset($evento['fields']['date_time']['end_date']))
-                            <img src="{{ get_template_directory_uri() }}/resources/images/svg/icon/caret.svg" class="w-3 h-3 stroke-blue -rotate-90" />
+    <section id="{{ $block['id'] }}">
+        <div class="glide" data-glide='{
+            "loop": true,
+            "perView": 4
+        }'>
+        <div class="glide__track" data-glide-el="track">
+            <ul class="glide__slides">
+                @foreach ($eventos as $evento)
+                
+                <li class="flex flex-col rounded-xl bg-gray-100 glide__slide">
+                    <div class="rounded-l-xl text-left p-5 flex flex-col justify-center">
+                        <div class="flex flex-row items-center justify-between">
+                            @if (isset($evento['fields']['date_time']['start_date']))
                             <div class="h-16 w-24 text-base uppercase rounded-lg border border-blue/20 bg-blue/5 flex flex-col items-center justify-center">
-                                <span class="{{ isset($evento['fields']['date_time']['end_hour']) ? 'border-b border-blue/40' : '' }}">
-                                    {{ $evento['fields']['date_time']['end_date'] }}
-                                </span>
-                                @if ($evento['fields']['date_time']['end_hour'])
+                                <p class="{{ isset($evento['fields']['date_time']['start_hour']) ? 'border-b border-blue/40' : '' }}">
+                                    {{ $evento['fields']['date_time']['start_date'] }}
+                                </p>
+                                @if ($evento['fields']['date_time']['start_hour'])
                                     <p>
-                                        {{ $evento['fields']['date_time']['end_hour'] }}
+                                        {{ $evento['fields']['date_time']['start_hour'] }}
                                     </p>
                                 @endif
                             </div>
-                        @endif
-                    </div>
-                    
-                    <div>
-                        <h4 class="text-blue text-left pt-5">{{ $evento['title'] }}</h4>
-                        @if(isset($evento['fields']['info']))
-                        <p class="text-sm text-left pt-3">
-                            {{ $evento['fields']['info'] }}
-                        </p>
-                        @endif
-                    </div>
-                </li>
-            @endforeach
-        </ul>
-    </div>
-    <div class="glide__arrows" data-glide-el="controls">
-        <button class="glide__arrow glide__arrow--left" data-glide-dir="&lt;"></button>
-        <button class="glide__arrow glide__arrow--right" data-glide-dir="&gt;"></button>
-    </div>
-</section>
+                            @endif
+                            
+                            @if (isset($evento['fields']['date_time']['end_date']))
+                                <img src="{{ get_template_directory_uri() }}/resources/images/svg/icon/caret.svg" class="w-3 h-3 stroke-blue -rotate-90" />
+                                <div class="h-16 w-24 text-base uppercase rounded-lg border border-blue/20 bg-blue/5 flex flex-col items-center justify-center">
+                                    <span class="{{ isset($evento['fields']['date_time']['end_hour']) ? 'border-b border-blue/40' : '' }}">
+                                        {{ $evento['fields']['date_time']['end_date'] }}
+                                    </span>
+                                    @if ($evento['fields']['date_time']['end_hour'])
+                                        <p>
+                                            {{ $evento['fields']['date_time']['end_hour'] }}
+                                        </p>
+                                    @endif
+                                </div>
+                            @endif
+                        </div>
+                        
+                        <div>
+                            <h4 class="text-blue text-left pt-5">{{ $evento['title'] }}</h4>
+                            @if(isset($evento['fields']['info']))
+                            <p class="text-sm text-left pt-3">
+                                {{ $evento['fields']['info'] }}
+                            </p>
+                            @endif
+                        </div>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+        <div class="glide__arrows" data-glide-el="controls">
+            <button class="glide__arrow glide__arrow--left" data-glide-dir="&lt;"></button>
+            <button class="glide__arrow glide__arrow--right" data-glide-dir="&gt;"></button>
+        </div>
+    </section>
 
-<style type="text/css">
-  p#{{ $block['id'] }} {
-    {{ $style }}
-  }
-</style> 
+    <style type="text/css">
+    p#{{ $block['id'] }} {
+        {{ $style }}
+    }
+    </style> 
+
+@endif
+
+<style>
+    .block-calendario:not(:has(.glide)) {
+        display: none;
+    }
+</style>
